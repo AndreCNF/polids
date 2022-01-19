673fed6ed866e80a2ffb2f040a25e59e4d4779c5
AndreCNF <andrecnf@gmail.com>
2022-01-19 19:44:17 +0000

    Plot topic presence

--- utils/nlp_utils.py
+++ utils/nlp_utils.py
@@ -1,9 +1,11 @@
-from typing import List
+from typing import Dict, List
 import os
 import matplotlib.pyplot as plt
+import plotly.express as px
 from wordcloud import WordCloud
 import spacy
 from string import punctuation
+import pandas as pd
 
 SPACY_NLP = spacy.load("pt_core_news_lg")
 
@@ -121,7 +123,7 @@ def get_words(text: str) -> List[str]:
     """
     doc = SPACY_NLP(text)
     words = [
-        word.text.replace("\n", "")  # remove new line symbols
+        word.text.replace("\n", "").replace("*", "")  # remove new line and bold symbols
         for word in doc
         if not word.is_stop  # remove stopwords
         and not word.is_punct  # remove punctuation
@@ -136,3 +138,63 @@ def get_words(text: str) -> List[str]:
     # remove blank words
     words = [word for word in words if word != ""]
     return words
+
+
+def get_topical_sentences(
+    sentences: List[str], topics: Dict[str, List[str]]
+) -> Dict[str, List[str]]:
+    """
+    Get lists of sentences per topic.
+
+    Args:
+        sentences (List[str]):
+            List of sentences to analyse.
+        topics (Dict[str, List[str]]):
+            Dictionary of words per topic.
+
+    Returns:
+        Dict[str, List[str]]:
+            Dictionary of sentences per topic.
+    """
+    topical_sentences = dict()
+    for topic in topics:
+        topical_sentences[topic] = list()
+    for sentence in sentences:
+        for topic in topics:
+            if any(topical_word in sentence for topical_word in topics[topic]):
+                topical_sentences[topic].append(sentence)
+    return topical_sentences
+
+
+def plot_topical_presence(
+    sentences: List[str], topics: Dict[str, List[str]], title: str = None
+):
+    """
+    Plot the number of sentences per topic.
+
+    Args:
+        sentences (List[str]):
+            List of sentences to analyse.
+        topics (Dict[str, List[str]]):
+            Dictionary of words per topic.
+    """
+    topical_sentences = get_topical_sentences(sentences, topics)
+    topic_sentence_count = dict()
+    for topic in topical_sentences.keys():
+        topic_sentence_count[topic] = len(topical_sentences[topic])
+    topic_sentence_count = pd.DataFrame(
+        topic_sentence_count, index=["sentence_count"]
+    ).T
+    topic_sentence_count["sentence_percentage"] = (
+        topic_sentence_count["sentence_count"] / len(sentences) * 100
+    )
+    topic_sentence_count.index.name = "topic"
+    topic_sentence_count.sort_index(inplace=True)
+    fig = px.bar(topic_sentence_count, x="sentence_percentage", orientation="h")
+    fig.update_layout(
+        title=title,
+        xaxis_title="Percentagem de frases topicais no texto",
+        yaxis_title="Tópico",
+        yaxis=dict(categoryorder="category descending"),
+        # marker_color="rgb(0, 0, 0)",
+    )
