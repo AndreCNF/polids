90861f58ee8a4e5b4dbe76aae1257b06010f77fc
AndreCNF <andrecnf@gmail.com>
2022-01-23 19:22:51 +0000

    Update hyperlink

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ PAGES = dict(Geral=layouts.geral, Individual=layouts.individual)
 st.sidebar.title("Polids | 🇵🇹 2022")
 st.sidebar.markdown("Análise de dados dos programas políticos a eleições.")
 st.sidebar.markdown(
-    "[Mais info](https://github.com/AndreCNF/elections-analysis)",
+    "[Mais info](https://github.com/AndreCNF/polids)",
     unsafe_allow_html=True,
 )
 st.sidebar.title("Navegação")
